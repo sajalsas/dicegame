82a5b1ed4fb3b60ea6f81d58ea72ed4167f5d867
sajalsas <118689240+sajalsas@users.noreply.github.com>
2023-06-08 23:10:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomDiceImage = "/blob/main/dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
 var randomImageSource = randomDiceImage; //images/dice1.png - images/dice6.png
 
@@ -12,7 +12,7 @@ image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "/blob/main/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
